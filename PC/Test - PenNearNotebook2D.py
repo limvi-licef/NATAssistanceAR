4c185258df6e0e1a15bda13d9446df6a4203190d
anthony.melin <anthony.melin@laposte.net>
2020-04-07 18:05:12 +0200

    commit projet annuel m1 - 7/4/20

---
+++ PC/Test - PenNearNotebook2D.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# -------------------------
+# ### Author: Thomas Leonardon, Pierre-Baptiste Cougnenc, Dylan Mielot, Anthony Melin
+# ### Date: 2020 April 5
+# -------------------------
+
+# # Import
+
+
+from Tensorflow.RemoteCam import RemoteCam
+from Tensorflow.ObjectDetection import *
+from Tensorflow.ObjectDetector import ObjectDetector
+
+from Scenario.PenNearNotebook2D import PenNearNotebook2D
+
+import cv2
+import sys
+import numpy as np
+
+
+# # Global variables
+# ## Model files and classes
+
+MODEL_DIRECTORY = "E:/Projet annuel M1/HoloObjectDetection/PC/models/model_3"
+
+GRAPH = MODEL_DIRECTORY + "/frozen_inference_graph.pb"
+LABELS = MODEL_DIRECTORY + "/labelmap.pbtxt"
+NUM_CLASSES = 99
+
+
+# ## Scenario
+
+scenario = PenNearNotebook2D()
+
+
+# # Object detection configuration
+
+# ### Load model
+
+category_index = load_categories(LABELS, NUM_CLASSES)
+sess, inputs, outputs = load_model(GRAPH)
+
+
+# ### Set frame detector
+
+detector = ObjectDetector(sess, inputs, outputs, category_index)
+detector.SetThreshold(60)
+detector.draw = True
+
+
+# # Loop
+
+camera = RemoteCam(10000, 3)
+
+while True:
+
+    # frame
+    frame = camera.getFrame()
+    
+    if frame.shape == (720, 1280, 3):
+        
+        # detection
+        detected = detector.Detect(frame)
+        scenario.Update(detected, frame)
+    
+    # display
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) == ord('q'): break
+
+cv2.destroyAllWindows()
+camera.close()
